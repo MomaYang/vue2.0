a60a0bec478e0a729ce5791a049bf10653dcdc17
MomaYang <798336856@qq.com>
2019-04-25 23:03:39 +0800

    数据和方法

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 //实例化vue对象  new Vue({});
 
-new Vue({  //对象
+new Vue({
 	el: "#vue-app",
 	data: {
-		name: "摹码"
+		name: "摹码",
+		job: "web开发"
+	},
+	methods: {
+		greet: function(time){
+			return 'Good'+time+' '+this.name+'!';
+		}
 	}
 });
 
@@ -11,7 +17,7 @@ new Vue({  //对象
 /*
 	el: element 需要获取的元素，一定是html中的根容器元素
 	data: 用于数据的存储，对象，数组，字符串，nuber等
+	
 
-
-
+	this: 指的是实例化vue对象
 */
