479b08bff695623b38b1dab60480300cb7243db5
MomaYang <798336856@qq.com>
2019-04-25 22:46:29 +0800

    实例化vue对象

---
+++ app.js
@@ -0,0 +1,17 @@
+//实例化vue对象  new Vue({});
+
+new Vue({  //对象
+	el: "#vue-app",
+	data: {
+		name: "摹码"
+	}
+});
+
+
+/*
+	el: element 需要获取的元素，一定是html中的根容器元素
+	data: 用于数据的存储，对象，数组，字符串，nuber等
+
+
+
+*/
