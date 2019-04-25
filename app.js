7bf397460fea7e64be12aeb4b55e7b0f3f22024c
MomaYang <798336856@qq.com>
2019-04-25 23:22:40 +0800

    vue属性绑定

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ new Vue({
 	el: "#vue-app",
 	data: {
 		name: "摹码",
-		job: "web开发"
+		job: "web开发",
+		website: "http://www.baidu.com",
+		websiteTag: "<a href='http://www.baidu.com'>绑定标签</a>"
 	},
 	methods: {
 		greet: function(time){
@@ -17,7 +19,8 @@ new Vue({
 /*
 	el: element 需要获取的元素，一定是html中的根容器元素
 	data: 用于数据的存储，对象，数组，字符串，nuber等
-	
+	methods: 用于存储各种方法
+	data-binding: 给属性绑定对应的值
 
 	this: 指的是实例化vue对象
 */
