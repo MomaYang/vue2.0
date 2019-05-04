83bfd800be90eda84f3f8a689078b8ea88b8dcac
MomaYang <798336856@qq.com>
2019-05-04 22:26:21 +0800

    动态绑定样式

--- app.js
+++ app.js
@@ -3,28 +3,18 @@
 new Vue({
 	el: "#vue-app",
 	data: {
-		a: 0,
-		b: 0,
-		age: 20
+		changeColor: false,
+		changeLength: false
 	},
 	methods: {
-		// addToA: function(){
-		// 	console.log("add to a");
-		// 	return this.a + this.age;
-		// },
-		// addToB: function(){
-		// 	console.log("add to b");
-		// 	return this.b + this.age;
-		// }
+
 	},
 	computed: {
-		addToA: function(){
-            console.log('addToA');
-            return this.a + this.age;
-        },
-        addToB: function(){
-            console.log('addToB');
-            return this.b + this.age;
-        }
+		comClass:function(){
+			return {
+				changeColor: this.changeColor,
+				changeLength: this.changeLength
+			}
+		}
 	}
 })
